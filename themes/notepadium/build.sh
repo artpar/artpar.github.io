458ba40d719863cf8bde5794146917dbd91d1536
artpar <artpar@gmail.com>
2023-09-04 12:50:05 +0530

    delete hugo book

--- themes/notepadium/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-wget https://files.stork-search.net/releases/latest/stork-ubuntu-latest
-chmod +x stork-ubuntu-latest
-hugo --gc --minify -s exampleSite
-./stork-ubuntu-latest --build exampleSite/public/index.toml
